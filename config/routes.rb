92a32d78dcac3eb7379183a0421190d4a414e17c
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2019-10-25 12:04:46 -0500

    Modulo Estudiantes
    Se termina de desarrollar el módulo de estudiantes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :enrollments, only: %w[new, create, destroy]
+  resources :enrollments, only: [:new, :create, :destroy]
   resources :students
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
